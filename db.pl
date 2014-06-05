f803818e8b8d63eed886e3f0af0fd943399a7b01
massyn <massyn@gmail.com>
2014-06-06 09:10:33 +1000

    Initial data file

---
+++ db.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+
+# configure this file with your mySQL system details.
+# You must have a database already created
+
+# Please do not use root !!
+
+sub dbconnect
+{
+	# hostname, database, username, password
+	return ('localhost','yubiserver','root','pass123');
+}
+
+
